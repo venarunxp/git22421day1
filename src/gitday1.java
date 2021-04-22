b0a52a6ad4ba13d43dd833006b0be580f73656a1
venarunxp <venarunxp@gmail.com>
2021-04-22 19:45:57 +0530

    My Task is Done

---
+++ src/gitday1.java
@@ -0,0 +1,13 @@
+
+public class gitday1 {
+
+	public static void main(String[] args) {
+		int x=3,y;
+		String name = "Arun";
+		while(x<20){
+			y = x++;
+			System.out.println(name);
+				
+	}
+	}
+}
